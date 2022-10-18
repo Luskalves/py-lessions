87805499b479bcd92bb32be439c2fadbfef34fd2
Luskalves <lusca.alves32@gmail.com>
2022-10-18 18:43:11 -0300

    try/except adcionado

--- aulas/semana1/dia2/personagens/personagens_legais.py
+++ aulas/semana1/dia2/personagens/personagens_legais.py
@@ -1,13 +1,23 @@
-eu_literalmente = ['Coringa (2019)\n', 'Scarface\n']
+eu_literalmente = ["Coringa (2019)\n", "Scarface\n"]
 
-with open('literalmente_eu.txt', mode='w') as file:
-    file.write('Patrick Bateman\n')
-    file.write('Batman (Robert Pattinson)\n')
-    file.write('Agente K\n')
-    file.write('Driver\n')
-    print('Ikari Shinji', file=file)
-    file.writelines(eu_literalmente)
+try:
+    with open("literalmente_eu.txt", mode="w") as file:
+        file.write("Patrick Bateman\n")
+        file.write("Batman (Robert Pattinson)\n")
+        file.write("Agente K\n")
+        file.write("Driver\n")
+        print("Ikari Shinji", file=file)
+        file.writelines(eu_literalmente)
+except TypeError:
+    print('Erro de tipo ao abrir o arquivo!')
+except FileNotFoundError:
+    print("Falha ao criar o arquivo!")
+except ValueError:
+    print("Falha ao escolher o modo de leitura/escrita")
 
-with open('literalmente_eu.txt', mode="r") as file:
-    for line in file:
-        print(line)
+try:
+    with open("literalmente_eu.txt", mode="r") as file:
+        for line in file:
+            print(line)
+except FileNotFoundError:
+    print("Falha ao tentar ler o arquivo! inexistente/não encontrado")
