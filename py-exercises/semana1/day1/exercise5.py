dd15ee58e240253030634ba45668ee8d1aebd212
Luskalves <lusca.alves32@gmail.com>
2022-10-20 08:28:00 -0300

    deu nn

---
+++ py-exercises/semana1/day1/exercise5.py
@@ -0,0 +1,13 @@
+# def predrero(size):
+#     litros = size / 3
+#     print(litros)
+#     latas = litros / 18.0
+#     print(latas)
+#     preco = latas * 80.0
+#     return (latas, preco)
+
+
+# info = predrero(10)
+# print(info)
+
+# INFELIZMENTE É MAIS FÁCIL CONTRUIR UMA API DE UM BANCO CHEIO DE TABELAS RELACIONAIS QUE ESSE EXERCÍCIO
